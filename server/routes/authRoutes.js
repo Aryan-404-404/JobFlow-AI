80982a7f992557e06ab0b65337fcd16612c836c2
Aryan <aryan411770@gmail.com>
2026-02-09 10:48:05 +0530

    Changed the secure as true in cookies

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -40,7 +40,7 @@ router.get(
         );
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
+            secure: true,
             sameSite: 'None',
             maxAge: 30 * 24 * 60 * 60 * 1000
         })
